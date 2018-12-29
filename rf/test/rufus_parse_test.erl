8807ccc9e7b906a35119ec16369481c78c735d27
Jamu Kakar <jkakar@kakar.ca>
2018-12-28 20:32:55 -0800

    tests: Unify tests for the same module into one file each

--- rf/test/rufus_parse_test.erl
+++ rf/test/rufus_parse_test.erl
@@ -1,15 +1,26 @@
--module(rufus_parse_package_test).
+-module(rufus_parse_test).
 
 -include_lib("eunit/include/eunit.hrl").
 
-empty_package_test() ->
+parse_empty_package_test() ->
     {ok, Tokens, _} = rufus_scan:string("package empty"),
     {ok, AST} = rufus_parse:parse(Tokens),
     [
      {package, 1, "empty"}
     ] = AST.
 
-import_test() ->
+parse_function_returning_an_int_test() ->
+    {ok, Tokens, _} = rufus_scan:string("
+     package rand
+     func Int() int { 42 }
+    "),
+    {ok, AST} = rufus_parse:parse(Tokens),
+    [
+     {package, 2, "rand"},
+     {func, 3, "Int", [], int, [{expr,3,{int,42}}]}
+    ] = AST.
+
+parse_import_test() ->
     {ok, Tokens, _} = rufus_scan:string("
 package foo
 import \"bar\"
