057675f71861b67306b6dea717b2eac3bc5af1e1
Jamu Kakar <jkakar@kakar.ca>
2019-01-01 21:24:40 -0800

    misc: Rename type checker module

--- rf/src/rufus_check_types.erl
+++ rf/src/rufus_check_types.erl
@@ -1,33 +1,33 @@
-%% rufus_return_type enforces the invariant that the type of a function's return
+%% rufus_check_types enforces the invariant that the type of a function's return
 %% value matches the return type specified in the function signature.
--module(rufus_return_type).
+-module(rufus_check_types).
 
 %% API exports
 
--export([check/1]).
+-export([forms/1]).
 
 %% API
 
-%% check iterates over Rufus forms and determines whether the type of each
+%% forms iterates over RufusForms and determines whether the type of each
 %% function return value matches the return type defined in the function
 %% signature. Iteration stops at the first error. Return values:
-%% - `ok` if no issues are found.
-%% - `{error, unmatched_return_type, Data}` with `Data` containing
-%%   `actual_return_value` and `expected_return_value` atom keys pointing to
-%%   Rufus types if return value types are unmatched.
-check(Forms) ->
-    check(Forms, Forms).
+%% - `{ok, RufusForms}` if no issues are found.
+%% - `{error, unmatched_return_type, Data}` with `Data` containing `actual` and
+%%   `expected` atom keys pointing to Rufus types if return value types are
+%%   unmatched.
+forms(RufusForms) ->
+    forms(RufusForms, RufusForms).
 
 %% Private API
 
-check(Forms, [H|T]) ->
+forms(Forms, [H|T]) ->
     case check_expr(H) of
         ok ->
-            check(Forms, T);
+            forms(Forms, T);
         Error ->
             Error
     end;
-check(Forms, []) ->
+forms(Forms, []) ->
     {ok, Forms}.
 
 check_expr({func, _LineNumber, _Name, _Arguments, ReturnType, Exprs}) ->
