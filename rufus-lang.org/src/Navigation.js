72e4ffa73f9ff354f728ab4b4b02cb50fd34443a
Jamu Kakar <jkakar@kakar.ca>
2019-10-18 21:04:18 -0700

    wip

---
+++ rufus-lang.org/src/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Navigation.css';
+
+function Navigation() {
+    return (
+        <header className="Navigation-header">
+            <div class="Navigation-container">
+                <a class="Navigation-logo" href="/">Rufus</a>
+            </div>
+        </header>
+    );
+}
+
+export default Navigation;
---
+++ rufus-lang.org/src/Page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Page.css';
+
+function Page() {
+    return (
+        <div className="Page">
+
+        </div>
+    );
+}
+
+export default Page;
