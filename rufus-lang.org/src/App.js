318c993a2c35050c002c001f2de61eafab0fe873
Jamu Kakar <jkakar@kakar.ca>
2019-10-19 20:40:44 -0700

    Move header, main and footer components into new layout directory.

--- rufus-lang.org/src/App.js
+++ rufus-lang.org/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Footer from './Footer';
-import Header from './Header';
-import Main from './Main';
+import Footer from './layout/Footer';
+import Header from './layout/Header';
+import Main from './layout/Main';
 
 import './App.css';
 
