5783419f6b07e2f77d8b401b80dac28f5d0906fb
Jamu Kakar <jkakar@kakar.ca>
2019-10-21 15:21:06 -0700

    Add more tests.

--- rf/test/rufus_typecheck_apply_test.erl
+++ rf/test/rufus_typecheck_apply_test.erl
@@ -2,67 +2,16 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-%% Arity-0 functions calling an arity-0 function
-
-forms_with_function_calling_a_function_without_arguments_test() ->
+forms_with_function_calling_an_unknown_function_test() ->
     RufusText = "
-    module math
-    func Four() int { 100 % 13 % 5 }
-    func Random() int { Four() }
+    module example
+    func Echo(text string) string { Ping() }
     ",
     {ok, Tokens} = rufus_tokenize:string(RufusText),
     {ok, Forms} = rufus_parse:parse(Tokens),
-    {ok, AnnotatedForms1} = rufus_annotate_locals:forms(Forms),
-    {ok, AnnotatedForms2} = rufus_typecheck_binary_op:forms(AnnotatedForms1),
-    {ok, AnnotatedForms3} = rufus_typecheck_apply:forms(AnnotatedForms2),
-    Expected = [{module, #{line => 2,
-                           spec => math}},
-                {func_decl, #{args => [],
-                              exprs => [{binary_op, #{left => {binary_op, #{left => {int_lit, #{line => 3,
-                                                                                                spec => 100,
-                                                                                                type => {type, #{line => 3,
-                                                                                                                 source => inferred,
-                                                                                                                 spec => int}}}},
-                                                                            line => 3,
-                                                                            op => '%',
-                                                                            right => {int_lit, #{line => 3,
-                                                                                                 spec => 13,
-                                                                                                 type => {type, #{line => 3,
-                                                                                                                  source => inferred,
-                                                                                                                  spec => int}}}},
-                                                                            type => {type, #{line => 3,
-                                                                                             source => inferred,
-                                                                                             spec => int}}}},
-                                                      line => 3,
-                                                      locals => #{},
-                                                      op => '%',
-                                                      right => {int_lit, #{line => 3,
-                                                                           spec => 5,
-                                                                           type => {type, #{line => 3,
-                                                                                            source => inferred,
-                                                                                            spec => int}}}},
-                                                      type => {type, #{line => 3,
-                                                                       source => inferred,
-                                                                       spec => int}}}}],
-                              line => 3,
-                              return_type => {type, #{line => 3,
-                                                      source => rufus_text,
-                                                      spec => int}},
-                              spec => 'Four'}},
-                {func_decl, #{args => [],
-                              exprs => [{apply, #{args => [],
-                                                  line => 4,
-                                                  locals => #{},
-                                                  spec => 'Four',
-                                                  type => {type, #{line => 3,
-                                                                   source => rufus_text,
-                                                                   spec => int}}}}],
-                              line => 4,
-                              return_type => {type, #{line => 4,
-                                                      source => rufus_text,
-                                                      spec => int}},
-                              spec => 'Random'}}],
-    ?assertEqual(Expected, AnnotatedForms3).
+    Result = rufus_typecheck_apply:forms(Forms),
+    Data = #{args => [], spec => 'Ping'},
+    ?assertEqual({error, unknown_func, Data}, Result).
 
 forms_with_function_calling_a_function_with_a_missing_argument_test() ->
     RufusText = "
@@ -91,13 +40,81 @@ forms_with_function_calling_a_function_with_a_missing_argument_test() ->
                                           spec => 'Echo'}}]},
     ?assertEqual({error, unknown_arity, Data}, Result).
 
-%% forms_with_function_calling_a_function_with_a_mismatched_argument_type_test() ->
-%%     RufusText = "
-%%     module example
-%%     func Echo(n string) string { \"Hello\" }
-%%     func Broken() string { Echo(42) }
-%%     ",
-%%     {ok, Tokens} = rufus_tokenize:string(RufusText),
-%%     {ok, Forms} = rufus_parse:parse(Tokens),
-%%     Result = rufus_typecheck_apply:forms(Forms),
-%%     ?assertEqual({error, invalid_arg_type, #{}}, Result).
+forms_with_function_calling_a_function_with_a_mismatched_argument_type_test() ->
+    RufusText = "
+    module example
+    func Echo(n string) string { \"Hello\" }
+    func Broken() string { Echo(42) }
+    ",
+    {ok, Tokens} = rufus_tokenize:string(RufusText),
+    {ok, Forms} = rufus_parse:parse(Tokens),
+    Result = rufus_typecheck_apply:forms(Forms),
+    Data = #{arg_exprs => [{int_lit, #{line => 4,
+                                       spec => 42,
+                                       type => {type, #{line => 4,
+                                                        source => inferred,
+                                                        spec => int}}}}],
+             form_decls => [{func_decl, #{args => [{arg_decl, #{line => 3,
+                                                                spec => n,
+                                                                type => {type, #{line => 3,
+                                                                                 source => rufus_text,
+                                                                                 spec => string}}}}],
+                                          exprs => [{string_lit, #{line => 3,
+                                                                   spec => <<"Hello">>,
+                                                                   type => {type, #{line => 3,
+                                                                                    source => inferred,
+                                                                                    spec => string}}}}],
+                                          line => 3,
+                                          return_type => {type, #{line => 3,
+                                                                  source => rufus_text,
+                                                                  spec => string}},
+                                          spec => 'Echo'}}]},
+    ?assertEqual({error, unmatched_args, Data}, Result).
+
+forms_with_function_calling_a_function_with_one_argument_test() ->
+    RufusText = "
+    module math
+    func Echo(text string) string { text }
+    func Greeting() string { Echo(\"hello\") }
+    ",
+    {ok, Tokens} = rufus_tokenize:string(RufusText),
+    {ok, Forms} = rufus_parse:parse(Tokens),
+    {ok, AnnotatedForms1} = rufus_annotate_locals:forms(Forms),
+    {ok, AnnotatedForms2} = rufus_typecheck_apply:forms(AnnotatedForms1),
+    Expected = [{module,#{line => 2,
+                          spec => math}},
+                {func_decl, #{args => [{arg_decl, #{line => 3,
+                                                    spec => text,
+                                                    type => {type, #{line => 3,
+                                                                     source => rufus_text,
+                                                                     spec => string}}}}],
+                              exprs => [{identifier, #{line => 3,
+                                                       locals => #{text => {type, #{line => 3,
+                                                                                    source => rufus_text,
+                                                                                    spec => string}}},
+                                                       spec => text}}],
+                              line => 3,
+                              return_type => {type, #{line => 3,
+                                                      source => rufus_text,
+                                                      spec => string}},
+                              spec => 'Echo'}},
+                {func_decl, #{args => [],
+                              exprs => [{apply, #{args => [{string_lit, #{line => 4,
+                                                                          spec => <<"hello">>,
+                                                                          type => {type, #{line => 4,
+                                                                                           source => inferred,
+                                                                                           spec => string}}}}],
+                                                  line => 4,
+                                                  locals => #{text => {type, #{line => 3,
+                                                                               source => rufus_text,
+                                                                               spec => string}}},
+                                                  spec => 'Echo',
+                                                  type => {type, #{line => 3,
+                                                                   source => rufus_text,
+                                                                   spec => string}}}}],
+                              line => 4,
+                              return_type => {type, #{line => 4,
+                                                      source => rufus_text,
+                                                      spec => string}},
+                              spec => 'Greeting'}}],
+    ?assertEqual(Expected, AnnotatedForms2).
