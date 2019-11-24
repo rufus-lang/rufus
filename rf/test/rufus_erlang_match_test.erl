ec735a0d88ed87e3edf02cba20f8adb32e76f5b6
Jamu Kakar <jkakar@kakar.ca>
2019-11-23 22:32:49 -0800

    compiler: Compile match expressions (#69)
    `rufus_erlang` transforms `match` forms into Erlang forms. `rufus_expr` returns
    an `illegal_pattern` error if the left hand side of a `match` expression
    contains a `call` form. `match` expressions can be compiled and run.

---
+++ rf/test/rufus_erlang_match_test.erl
@@ -0,0 +1,103 @@
+-module(rufus_erlang_match_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+forms_for_function_with_a_match_that_binds_an_atom_literal_test() ->
+    RufusText = "
+    module example
+    func Ping() atom {
+        response = :pong
+        response
+    }
+    ",
+    {ok, Tokens} = rufus_tokenize:string(RufusText),
+    {ok, Forms} = rufus_parse:parse(Tokens),
+    {ok, AnnotatedForms} = rufus_expr:typecheck_and_annotate(Forms),
+    {ok, ErlangForms} = rufus_erlang:forms(AnnotatedForms),
+    Expected = [{attribute, 2, module, example},
+                {attribute, 3, export, [{'Ping', 0}]},
+                {function, 3, 'Ping', 0,
+                 [{clause, 3, [], [], [{match, 4, {var, 4, response}, {atom, 4, pong}},
+                                       {var, 5, response}]}]}],
+    ?assertEqual(Expected, ErlangForms).
+
+forms_for_function_with_a_match_that_binds_a_bool_literal_test() ->
+    RufusText = "
+    module example
+    func Truthy() bool {
+        response = true
+        response
+    }
+    ",
+    {ok, Tokens} = rufus_tokenize:string(RufusText),
+    {ok, Forms} = rufus_parse:parse(Tokens),
+    {ok, AnnotatedForms} = rufus_expr:typecheck_and_annotate(Forms),
+    {ok, ErlangForms} = rufus_erlang:forms(AnnotatedForms),
+    Expected = [{attribute, 2, module, example},
+                  {attribute, 3, export, [{'Truthy', 0}]},
+                  {function, 3, 'Truthy', 0, [
+                      {clause, 3, [], [], [{match, 4, {tuple, 4, [{atom, 4, bool}, {var, 4, response}]},
+                                                      {tuple, 4, [{atom, 4, bool}, {atom, 4, true}]}},
+                                                      {tuple, 5, [{atom, 5, bool}, {var, 5, response}]}]}]}],
+    ?assertEqual(Expected, ErlangForms).
+
+forms_for_function_with_a_match_that_binds_a_float_literal_test() ->
+    RufusText = "
+    module example
+    func Pi() float {
+        response = 3.14159265359
+        response
+    }
+    ",
+    {ok, Tokens} = rufus_tokenize:string(RufusText),
+    {ok, Forms} = rufus_parse:parse(Tokens),
+    {ok, AnnotatedForms} = rufus_expr:typecheck_and_annotate(Forms),
+    {ok, ErlangForms} = rufus_erlang:forms(AnnotatedForms),
+    Expected = [{attribute, 2, module, example},
+                  {attribute, 3, export, [{'Pi', 0}]},
+                  {function, 3, 'Pi', 0, [
+                      {clause, 3, [], [], [{match, 4, {var, 4, response},
+                                                      {float, 4, 3.14159265359}},
+                                                      {var, 5, response}]}]}],
+    ?assertEqual(Expected, ErlangForms).
+
+forms_for_function_with_a_match_that_binds_an_int_literal_test() ->
+    RufusText = "
+    module example
+    func FortyTwo() int {
+        response = 42
+        response
+    }
+    ",
+    {ok, Tokens} = rufus_tokenize:string(RufusText),
+    {ok, Forms} = rufus_parse:parse(Tokens),
+    {ok, AnnotatedForms} = rufus_expr:typecheck_and_annotate(Forms),
+    {ok, ErlangForms} = rufus_erlang:forms(AnnotatedForms),
+    Expected = [{attribute, 2, module, example},
+                  {attribute, 3, export, [{'FortyTwo', 0}]},
+                  {function, 3, 'FortyTwo', 0, [
+                      {clause, 3, [], [], [{match, 4, {var, 4, response},
+                                                      {integer, 4, 42}},
+                                                      {var, 5, response}]}]}],
+    ?assertEqual(Expected, ErlangForms).
+
+forms_for_function_with_a_match_that_binds_a_string_literal_test() ->
+    RufusText = "
+    module example
+    func Greeting() string {
+        response = \"hello\"
+        response
+    }
+    ",
+    {ok, Tokens} = rufus_tokenize:string(RufusText),
+    {ok, Forms} = rufus_parse:parse(Tokens),
+    {ok, AnnotatedForms} = rufus_expr:typecheck_and_annotate(Forms),
+    {ok, ErlangForms} = rufus_erlang:forms(AnnotatedForms),
+    Expected = [{attribute, 2, module, example},
+                {attribute, 3, export, [{'Greeting', 0}]},
+                {function, 3, 'Greeting', 0,
+                 [{clause, 3, [], [], [{match, 4, {tuple, 4, [{atom, 4, string}, {var, 4, response}]},
+                                        {tuple, 4, [{atom, 4, string},
+                                                    {bin, 4, [{bin_element, 4, {string, 4, "hello"}, default, default}]}]}},
+                                       {tuple, 5, [{atom, 5, string}, {var, 5, response}]}]}]}],
+    ?assertEqual(Expected, ErlangForms).
