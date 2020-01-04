aaf3cd04a8768b6561689a5f10966bd2e98f14b5
Jamu Kakar <jkakar@kakar.ca>
2020-01-03 20:44:30 -0800

    Add more tests.

--- rf/test/rufus_expr_binary_op_test.erl
+++ rf/test/rufus_expr_binary_op_test.erl
@@ -2,9 +2,9 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-%% binary_op expressions
+%% arithmetic binary_op expressions
 
-typecheck_and_annotate_binary_op_with_ints_test() ->
+typecheck_and_annotate_arithmetic_binary_op_with_ints_test() ->
     RufusText = "
     module example
     func FortyTwo() int { 19 + 23 }
@@ -37,7 +37,7 @@ typecheck_and_annotate_binary_op_with_ints_test() ->
     {ok, AnnotatedForms} = rufus_expr:typecheck_and_annotate(Forms),
     ?assertEqual(Expected, AnnotatedForms).
 
-typecheck_and_annotate_binary_op_with_floats_test() ->
+typecheck_and_annotate_arithmetic_binary_op_with_floats_test() ->
     RufusText = "
     module example
     func Pi() float { 1.0 + 2.14159265359 }
@@ -70,7 +70,7 @@ typecheck_and_annotate_binary_op_with_floats_test() ->
     {ok, AnnotatedForms} = rufus_expr:typecheck_and_annotate(Forms),
     ?assertEqual(Expected, AnnotatedForms).
 
-typecheck_and_annotate_binary_op_with_float_and_int_test() ->
+typecheck_and_annotate_arithmetic_binary_op_with_float_and_int_test() ->
     RufusText = "
     module example
     func FortyTwo() int { 19.0 + 23 }
@@ -93,7 +93,7 @@ typecheck_and_annotate_binary_op_with_float_and_int_test() ->
     {error, unmatched_operand_type, Data} = rufus_expr:typecheck_and_annotate(Forms),
     ?assertEqual(Expected, Data).
 
-typecheck_and_annotate_binary_op_with_float_and_float_and_int_test() ->
+typecheck_and_annotate_arithmetic_binary_op_with_float_and_float_and_int_test() ->
     RufusText = "
     module example
     func FortyTwo() int { 13.0 + 6.0 + 23 }
@@ -126,7 +126,7 @@ typecheck_and_annotate_binary_op_with_float_and_float_and_int_test() ->
     {error, unmatched_operand_type, Data} = rufus_expr:typecheck_and_annotate(Forms),
     ?assertEqual(Expected, Data).
 
-typecheck_and_annotate_binary_op_with_bools_test() ->
+typecheck_and_annotate_arithmetic_binary_op_with_bools_test() ->
     RufusText = "
     module example
     func Concat() bool { true + false }
@@ -149,7 +149,7 @@ typecheck_and_annotate_binary_op_with_bools_test() ->
     {error, unsupported_operand_type, Data} = rufus_expr:typecheck_and_annotate(Forms),
     ?assertEqual(Expected, Data).
 
-typecheck_and_annotate_binary_op_with_strings_test() ->
+typecheck_and_annotate_arithmetic_binary_op_with_strings_test() ->
     RufusText = "
     module example
     func Concat() string { \"port\" + \"manteau\" }
@@ -173,9 +173,9 @@ typecheck_and_annotate_binary_op_with_strings_test() ->
     ?assertEqual(unsupported_operand_type, Reason),
     ?assertEqual(Expected, Data).
 
-%% binary_op expressions with the remainder operator
+%% arithmetic binary_op expressions with the remainder operator
 
-typecheck_and_annotate_remainder_binary_op_with_ints_test() ->
+typecheck_and_annotate_remainder_arithmetic_binary_op_with_ints_test() ->
     RufusText = "
     module example
     func Six() int { 27 % 7 }
@@ -208,7 +208,7 @@ typecheck_and_annotate_remainder_binary_op_with_ints_test() ->
     {ok, AnnotatedForms} = rufus_expr:typecheck_and_annotate(Forms),
     ?assertEqual(Expected, AnnotatedForms).
 
-typecheck_and_annotate_remainder_binary_op_with_floats_test() ->
+typecheck_and_annotate_remainder_arithmetic_binary_op_with_floats_test() ->
     RufusText = "
     module example
     func Six() int { 27.0 % 7.0 }
@@ -228,6 +228,63 @@ typecheck_and_annotate_remainder_binary_op_with_floats_test() ->
                                                               type => {type, #{line => 3,
                                                                                source => inferred,
                                                                                spec => float}}}}}}},
-    {error, Reason, Data} = rufus_expr:typecheck_and_annotate(Forms),
-    ?assertEqual(unsupported_operand_type, Reason),
+    {error, unsupported_operand_type, Data} = rufus_expr:typecheck_and_annotate(Forms),
+    ?assertEqual(Expected, Data).
+
+%% boolean binary_op expressions
+
+typecheck_and_annotate_boolean_binary_op_with_bools_test() ->
+    RufusText = "
+    module example
+    func Falsy() bool { true and false }
+    ",
+    {ok, Tokens} = rufus_tokenize:string(RufusText),
+    {ok, Forms} = rufus_parse:parse(Tokens),
+    Expected = [{module, #{line => 2,
+                           spec => example}},
+                {func, #{params => [],
+                         exprs => [{binary_op, #{left => {bool_lit, #{line => 3,
+                                                                      spec => true,
+                                                                      type => {type, #{line => 3,
+                                                                                       source => inferred,
+                                                                                       spec => bool}}}},
+                                                 line => 3,
+                                                 op => 'and',
+                                                 right => {bool_lit, #{line => 3,
+                                                                       spec => false,
+                                                                       type => {type, #{line => 3,
+                                                                                        source => inferred,
+                                                                                        spec => bool}}}},
+                                                 type => {type, #{line => 3,
+                                                                  source => inferred,
+                                                                  spec => bool}}}}],
+                         line => 3,
+                         return_type => {type, #{line => 3,
+                                                 source => rufus_text,
+                                                 spec => bool}},
+                         spec => 'Falsy'}}],
+    {ok, AnnotatedForms} = rufus_expr:typecheck_and_annotate(Forms),
+    ?assertEqual(Expected, AnnotatedForms).
+
+typecheck_and_annotate_boolean_binary_op_with_bool_and_int_test() ->
+    RufusText = "
+    module example
+    func Falsy() bool { true and 0 }
+    ",
+    {ok, Tokens} = rufus_tokenize:string(RufusText),
+    {ok, Forms} = rufus_parse:parse(Tokens),
+    Expected = #{form => {binary_op, #{left => {bool_lit, #{line => 3,
+                                                            spec => true,
+                                                            type => {type, #{line => 3,
+                                                                             source => inferred,
+                                                                             spec => bool}}}},
+                                       line => 3,
+                                       locals => #{},
+                                       op => 'and',
+                                       right => {int_lit, #{line => 3,
+                                                            spec => 0,
+                                                            type => {type, #{line => 3,
+                                                                             source => inferred,
+                                                                             spec => int}}}}}}},
+    {error, unsupported_operand_type, Data} = rufus_expr:typecheck_and_annotate(Forms),
     ?assertEqual(Expected, Data).
