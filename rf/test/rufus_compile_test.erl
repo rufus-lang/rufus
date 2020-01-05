5fa00acd126d6cdcb49f71ea9d081f5678b46b95
Jamu Kakar <jkakar@kakar.ca>
2020-01-04 20:14:24 -0800

    Rename rufus_compile:eval_chain/2 to rufus_compile:eval_stages/2.

--- rf/test/rufus_compile_test.erl
+++ rf/test/rufus_compile_test.erl
@@ -2,16 +2,16 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-eval_chain_without_handlers_test() ->
-    ?assertEqual({ok, "hello!"}, rufus_compile:eval_chain("hello!", [])).
+eval_stages_without_handlers_test() ->
+    ?assertEqual({ok, "hello!"}, rufus_compile:eval_stages("hello!", [])).
 
-eval_chain_with_ok_handler_test() ->
+eval_stages_with_ok_handler_test() ->
     Reverse = fun(Word) -> {ok, lists:reverse(Word)} end,
-    ?assertEqual({ok, "ahah"}, rufus_compile:eval_chain("haha", [Reverse])).
+    ?assertEqual({ok, "ahah"}, rufus_compile:eval_stages("haha", [Reverse])).
 
-eval_chain_with_error_handler_test() ->
+eval_stages_with_error_handler_test() ->
     Error = fun(_) -> {error, reason} end,
     Explode = fun(_) -> throw(explode) end,
-    %% Explode never runs because eval_chain stops iterating over handlers when
+    %% Explode never runs because eval_stages stops iterating over handlers when
     %% it encounters a failure from Error.
-    ?assertEqual({error, reason}, rufus_compile:eval_chain("haha", [Error, Explode])).
+    ?assertEqual({error, reason}, rufus_compile:eval_stages("haha", [Error, Explode])).
