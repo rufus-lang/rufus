488ab64896ba0e5090ad95e4b3ffb022ef6748ae
Jamu Kakar <jkakar@kakar.ca>
2020-02-18 19:54:18 -0800

    Fix Dialyzer issues.

--- rf/src/rufus_form.erl
+++ rf/src/rufus_form.erl
@@ -144,16 +144,9 @@ make_type(list, ElementType, Line) ->
 make_type_with_source(Spec, Source, Line) ->
     {type, #{spec => Spec, source => Source, line => Line}}.
 
-%% TODO(jkakar) Figure out why Dialyzer doesn't like this spec:
-%% -spec make_type_with_source(list, type_spec(), type_source(), integer()) -> type_form().
-make_type_with_source(list, ElementType = {type, #{spec := Spec}}, Source, Line) ->
-    TypeSpec = list_to_atom(unicode:characters_to_list(["list[", atom_to_list(Spec), "]"])),
-    {type, #{collection_type => list,
-             element_type => ElementType,
-             spec => TypeSpec,
-             source => Source,
-             line => Line}};
-make_type_with_source(list_lit, ElementType = {type, #{spec := Spec}}, Source, Line) ->
+-spec make_type_with_source(list | list_lit, type_form(), type_source(), integer()) -> type_form().
+make_type_with_source(_CollectionSpec, ElementType, Source, Line) ->
+    {type, #{spec := Spec}} = ElementType,
     TypeSpec = list_to_atom(unicode:characters_to_list(["list[", atom_to_list(Spec), "]"])),
     {type, #{collection_type => list,
              element_type => ElementType,
@@ -202,7 +195,7 @@ make_literal(list, Type, Elements, Line) ->
                  line => Line}}.
 
 %% make_cons returns a form for a cons expression.
--spec make_cons(type_form(), term(), list(term()), integer()) -> cons_form().
+-spec make_cons(type_form(), rufus_form(), list_lit_form(), integer()) -> cons_form().
 make_cons(Type, Head, Tail, Line) ->
     {cons, #{type => Type, head => Head, tail => Tail, line => Line}}.
 
