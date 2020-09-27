674522ba98315bbec082aebde976b072d08e1d15
Jamu Kakar <jkakar@kakar.ca>
2020-09-26 19:53:26 -0700

    Avoid hard-coding error code.

--- rf/src/rufus_expr.erl
+++ rf/src/rufus_expr.erl
@@ -296,8 +296,8 @@ typecheck_and_annotate_identifier(
                     AnnotatedForm2 = {identifier, Context2#{type => TypeForm}},
                     {ok, NewLocals} = push_local(Locals, AnnotatedForm2),
                     {ok, NewLocals, AnnotatedForm2};
-                {error, unknown_identifier, Data} ->
-                    throw({error, unknown_identifier, Data})
+                {error, Error, Data} ->
+                    throw({error, Error, Data})
             end;
         TypeForm ->
             AnnotatedForm2 = {identifier, Context1#{type => TypeForm}},
