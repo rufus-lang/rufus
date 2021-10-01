6716fea1f82f066e8f772750ef698dead05aaf57
Jamu Kakar <jkakar@kakar.ca>
2021-10-01 11:55:07 -0700

    Add tests.

--- rf/test/rufus_parse_case_test.erl
+++ rf/test/rufus_parse_case_test.erl
@@ -359,3 +359,167 @@ parse_function_with_case_block_with_single_cons_clause_test() ->
         }}
     ],
     ?assertEqual(Expected, Forms).
+
+parse_function_with_case_block_with_identifier_clause_test() ->
+    RufusText =
+        "func Maybe() atom {\n"
+        "    value = true\n"
+        "    case value {\n"
+        "    match true ->\n"
+        "        :ok\n"
+        "    }\n"
+        "}\n",
+    {ok, Tokens} = rufus_tokenize:string(RufusText),
+    {ok, Forms} = rufus_parse:parse(Tokens),
+    Expected = [
+        {func, #{
+            exprs =>
+                [
+                    {match_op, #{
+                        left => {identifier, #{line => 2, spec => value}},
+                        line => 2,
+                        right =>
+                            {bool_lit, #{
+                                line => 2,
+                                spec => true,
+                                type => {type, #{line => 2, spec => bool}}
+                            }}
+                    }},
+                    {'case', #{
+                        clauses =>
+                            [
+                                {case_clause, #{
+                                    exprs =>
+                                        [
+                                            {atom_lit, #{
+                                                line => 5,
+                                                spec => ok,
+                                                type =>
+                                                    {type, #{line => 5, spec => atom}}
+                                            }}
+                                        ],
+                                    line => 4,
+                                    match_expr =>
+                                        {bool_lit, #{
+                                            line => 4,
+                                            spec => true,
+                                            type =>
+                                                {type, #{line => 4, spec => bool}}
+                                        }}
+                                }}
+                            ],
+                        line => 3,
+                        match_expr =>
+                            {identifier, #{line => 3, spec => value}}
+                    }}
+                ],
+            line => 1,
+            params => [],
+            return_type => {type, #{line => 1, spec => atom}},
+            spec => 'Maybe'
+        }}
+    ],
+    ?assertEqual(Expected, Forms).
+
+parse_function_with_case_block_with_identifier_and_type_clause_test() ->
+    RufusText =
+        "func Echo(value bool) bool {\n"
+        "    case value {\n"
+        "    match value bool ->\n"
+        "        value\n"
+        "    }\n"
+        "}\n",
+    {ok, Tokens} = rufus_tokenize:string(RufusText),
+    {ok, Forms} = rufus_parse:parse(Tokens),
+    Expected = [
+        {func, #{
+            exprs =>
+                [
+                    {'case', #{
+                        clauses =>
+                            [
+                                {case_clause, #{
+                                    exprs =>
+                                        [{identifier, #{line => 4, spec => value}}],
+                                    line => 3,
+                                    match_expr =>
+                                        {param, #{
+                                            line => 3,
+                                            spec => value,
+                                            type =>
+                                                {type, #{line => 3, spec => bool}}
+                                        }}
+                                }}
+                            ],
+                        line => 2,
+                        match_expr =>
+                            {identifier, #{line => 2, spec => value}}
+                    }}
+                ],
+            line => 1,
+            params =>
+                [
+                    {param, #{
+                        line => 1,
+                        spec => value,
+                        type => {type, #{line => 1, spec => bool}}
+                    }}
+                ],
+            return_type => {type, #{line => 1, spec => bool}},
+            spec => 'Echo'
+        }}
+    ],
+    ?assertEqual(Expected, Forms).
+
+parse_function_with_case_block_with_match_clause_test() ->
+    RufusText =
+        "func Maybe() bool {\n"
+        "    case true {\n"
+        "    match value = true ->\n"
+        "        value\n"
+        "    }\n"
+        "}\n",
+    {ok, Tokens} = rufus_tokenize:string(RufusText),
+    {ok, Forms} = rufus_parse:parse(Tokens),
+    Expected = [
+        {func, #{
+            exprs =>
+                [
+                    {'case', #{
+                        clauses =>
+                            [
+                                {case_clause, #{
+                                    exprs =>
+                                        [{identifier, #{line => 4, spec => value}}],
+                                    line => 3,
+                                    match_expr =>
+                                        {match_op, #{
+                                            left =>
+                                                {identifier, #{line => 3, spec => value}},
+                                            line => 3,
+                                            right =>
+                                                {bool_lit, #{
+                                                    line => 3,
+                                                    spec => true,
+                                                    type =>
+                                                        {type, #{line => 3, spec => bool}}
+                                                }}
+                                        }}
+                                }}
+                            ],
+                        line => 2,
+                        match_expr =>
+                            {bool_lit, #{
+                                line => 2,
+                                spec => true,
+                                type => {type, #{line => 2, spec => bool}}
+                            }}
+                    }}
+                ],
+            line => 1,
+            params => [],
+            return_type => {type, #{line => 1, spec => bool}},
+            spec => 'Maybe'
+        }}
+    ],
+    ?assertEqual(Expected, Forms).
